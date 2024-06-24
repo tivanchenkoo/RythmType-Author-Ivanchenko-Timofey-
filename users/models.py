2408fd3ea240925ba5ca24c3b0d052159df4ac17
Stefan Corporation <abrams1234518@gmail.com>
2024-06-24 18:29:49 +0400

    Add field in to the user model

--- users/models.py
+++ users/models.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import AbstractUser
 
 class Users(AbstractUser):
     users_images = models.ImageField(upload_to='users_profile_images', null=True, blank=True)
+    completedLevels = models.CharField(max_length=2000, null=True, blank=True)
+
 
     def __str__(self):
         return f"User: {self.username} | ID: {self.id}"
